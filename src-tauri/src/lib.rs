a3819a8b5ded81aa14dda7c5bcc97283464426d1
Josh Thomas <josh@joshthomas.dev>
2025-12-03 11:06:30 -0600

    migrate SQL handling to frontend handling via drizzle

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,6 +2,13 @@ mod db;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let migrations = vec![Migration {
+        version: 1,
+        description: "create_draft_table",
+        sql: include_str!("../../src/lib/db/migrations/0000_clean_blockbuster.sql"),
+        kind: MigrationKind::Up,
+    }];
+
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(db::plugin())
