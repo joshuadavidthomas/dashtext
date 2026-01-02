16ed3b9d4ab515fca129da73ccf5421941e088cf
Josh Thomas <josh@joshthomas.dev>
2026-01-02 15:57:27 -0600

    fix missing vite wasm plugins

--- packages/desktop/vite.config.js
+++ packages/desktop/vite.config.js
@@ -2,14 +2,21 @@ import { readFileSync } from "fs";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 const pkg = JSON.parse(readFileSync("./package.json", "utf-8"));
 
 const host = process.env.TAURI_DEV_HOST;
 
 export default defineConfig(async () => ({
-  plugins: [tailwindcss(), sveltekit()],
+  plugins: [topLevelAwait(), wasm(), tailwindcss(), sveltekit()],
   define: { __APP_VERSION__: JSON.stringify(pkg.version) },
+  
+  optimizeDeps: {
+    exclude: ["@automerge/automerge-wasm"],
+  },
+  
   clearScreen: false,
   server: {
     port: 1420,
--- packages/web/vite.config.js
+++ packages/web/vite.config.js
@@ -2,12 +2,19 @@ import { readFileSync } from "fs";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 const pkg = JSON.parse(readFileSync("./package.json", "utf-8"));
 
 export default defineConfig(async () => ({
-  plugins: [tailwindcss(), sveltekit()],
+  plugins: [topLevelAwait(), wasm(), tailwindcss(), sveltekit()],
   define: { __APP_VERSION__: JSON.stringify(pkg.version) },
+  
+  optimizeDeps: {
+    exclude: ["@automerge/automerge-wasm"],
+  },
+  
   server: {
     fs: {
       allow: ["../.."]
